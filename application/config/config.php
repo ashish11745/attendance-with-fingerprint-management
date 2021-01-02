10d6faffaf2507fc036be6cb43ab02179063b0d0
ashish11745 <76877141+ashish11745@users.noreply.github.com>
2021-01-02 22:00:03 +0530

    Update config.php

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/system-management/'; //change link
+$config['base_url'] = 'http://localhost/attendance/'; //change link
 
 /*
 |--------------------------------------------------------------------------
